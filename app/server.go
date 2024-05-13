f174a3cc3f5223c28b3fb2686ce6c517ede07c0c
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-13 09:04:17 +0530

    Respond with 200

--- app/server.go
+++ app/server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
@@ -21,9 +23,24 @@ func main() {
 	}
 
 	// Accept a connection
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	// HTTP Response is made up of three parts, each separated by a [CRLF](https://developer.mozilla.org/en-US/docs/Glossary/CRLF) (`\r\n`):
+	// 1. Status line: `HTTP/1.1 200 OK`
+	// 2. One or more Headers: `Content-Type: text/html`
+	// 3. (Optional) Body: `<!DOCTYPE html><html><body><h1>Hello, World!</h1></body></html>`
+
+	// Create the HTTP response
+	statusLine := "HTTP/1.1 200 OK"
+	headers := ""
+	body := ""
+	CRLF := "\r\n"
+	response := strings.Join([]string{statusLine, headers, body}, CRLF)
+
+	// Respond to the connection
+	conn.Write([]byte(response))
 }
