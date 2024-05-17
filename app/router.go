e2cc74fd3b1337b5013600c92a2c258d4bc9f097
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-17 23:06:11 +0530

    Use `net/http` package

--- app/router.go
+++ app/router.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"net/http"
 	"strings"
 
 	handle "github.com/codecrafters-io/http-server-starter-go/app/handlers"
-	"github.com/codecrafters-io/http-server-starter-go/pkg/http"
+	httpMessage "github.com/codecrafters-io/http-server-starter-go/pkg/http"
 )
 
 // Route the request to the correct handler
-func route(req *http.Request, res *http.Response) {
+func route(req *httpMessage.Request, res *httpMessage.Response) {
 	switch {
 
 	// /files/{name}
@@ -25,11 +26,11 @@ func route(req *http.Request, res *http.Response) {
 
 	// /
 	case req.Path == "/":
-		res.WithStatus(200)
+		res.WithStatus(http.StatusOK)
 
 	// Default
 	default:
-		res.WithStatus(404)
+		res.WithStatus(http.StatusNotFound)
 	}
 
 }
