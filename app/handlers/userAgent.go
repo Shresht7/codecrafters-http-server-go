6fefa927a83a9946269b1be913a408feee3ae9f6
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-17 23:00:12 +0530

    Improve Comments

--- app/handlers/userAgent.go
+++ app/handlers/userAgent.go
@@ -6,15 +6,15 @@ import (
 	"github.com/codecrafters-io/http-server-starter-go/pkg/http"
 )
 
-// UserAgent handles the "/user-agent" endpoint.
-// It extracts the User-Agent header from the request headers and returns it as the response body.
-// If the User-Agent header is not found, it sets the response status to 404.
+// Handles the `/user-agent` endpoint.
+// Extracts the `User-Agent` header from the request headers and returns it as the response body.
+// If the `User-Agent` header is not found, sets the response status to 404.
 func UserAgent(req *http.Request, res *http.Response) {
 
-	// Extract the User-Agent header from the request headers
+	// Extract the `User-Agent` header from the request headers
 	userAgent, ok := req.Headers.Get("User-Agent")
 
-	// If the User-Agent header is not found, set the response status to 404
+	// If the `User-Agent` header is not found, set the response status to 404
 	if !ok {
 		res.WithStatus(404)
 		return
