6fbf138d63a10f534bc97c38c7f93c35b78bd9e6
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-17 23:04:54 +0530

    Use `net/http` package

--- app/handlers/userAgent.go
+++ app/handlers/userAgent.go
@@ -2,28 +2,29 @@ package handlers
 
 import (
 	"fmt"
+	"net/http"
 
-	"github.com/codecrafters-io/http-server-starter-go/pkg/http"
+	httpMessage "github.com/codecrafters-io/http-server-starter-go/pkg/http"
 )
 
 // Handles the `/user-agent` endpoint.
 // Extracts the `User-Agent` header from the request headers and returns it as the response body.
 // If the `User-Agent` header is not found, sets the response status to 404.
-func UserAgent(req *http.Request, res *http.Response) {
+func UserAgent(req *httpMessage.Request, res *httpMessage.Response) {
 
 	// Extract the `User-Agent` header from the request headers
 	userAgent, ok := req.Headers.Get("User-Agent")
 
 	// If the `User-Agent` header is not found, set the response status to 404
 	if !ok {
-		res.WithStatus(404)
+		res.WithStatus(http.StatusNotFound)
 		return
 	}
 
 	// Set the response status to 200, content type to "text/plain",
 	// content length to the length of the user agent, and body to the user agent
 	res.
-		WithStatus(200).
+		WithStatus(http.StatusOK).
 		WithHeaders(map[string]string{
 			"Content-Type":   "text/plain",
 			"Content-Length": fmt.Sprintf("%d", len(userAgent)),
