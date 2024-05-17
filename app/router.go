21e1ea315e0a45640dc5755a350dea7b89590f27
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-17 14:00:21 +0530

    Add spacing to improve readability

--- app/router.go
+++ app/router.go
@@ -10,15 +10,26 @@ import (
 // Route the request to the correct handler
 func route(req *http.Request, res *http.Response) {
 	switch {
-	case req.Path == "/":
-		res.WithStatus(200)
+
+	// /files/{name}
 	case strings.HasPrefix(req.Path, "/files/"):
 		handle.Files(req, res)
+
+	// /user-agent
 	case req.Path == "/user-agent":
 		handle.UserAgent(req, res)
+
+	// /echo/{str}
 	case strings.HasPrefix(req.Path, "/echo/"):
 		handle.Echo(req, res)
+
+	// /
+	case req.Path == "/":
+		res.WithStatus(200)
+
+	// Default
 	default:
 		res.WithStatus(404)
 	}
+
 }
