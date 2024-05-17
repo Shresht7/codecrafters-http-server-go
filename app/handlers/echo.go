cfaab534d168e8b9f7349d432148faa2c7d0ad97
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-05-17 18:03:41 +0530

    Refactor: Echo Handler

--- app/handlers/echo.go
+++ app/handlers/echo.go
@@ -9,12 +9,11 @@ import (
 	"github.com/codecrafters-io/http-server-starter-go/pkg/http"
 )
 
-// Echo handles the "/echo/{str}" endpoint.
-// It extracts the string from the request path and returns it as the response body.
-// If the string is not found in the request path, it sets the response status to 404.
+// Handles the `/echo/{str}` endpoint.
+// Extracts the string from the request path and returns it as the response body.
 func Echo(req *http.Request, res *http.Response) {
 
-	// Cut the prefix "/echo/" from the request path
+	// Remove the prefix `/echo/` from the request path (e.g. `/echo/hello` -> `hello`)
 	str, found := strings.CutPrefix(req.Path, "/echo/")
 
 	// If the prefix is not found, set the response status to 404
@@ -23,53 +22,53 @@ func Echo(req *http.Request, res *http.Response) {
 		return
 	}
 
-	// The content to be sent in the response
-	content := str
+	// If the request contains the `Accept-Encoding` header with the value "gzip"...
+	acceptEncoding, ok := req.Headers.Get("Accept-Encoding")
+	if ok && strings.Contains(acceptEncoding, "gzip") {
 
-	// If the request contains the header "Accept-Encoding" with the value "gzip",
-	// set the response header "Content-Encoding" to "gzip"
-	acceptableEncodings, ok := req.Headers.Get("Accept-Encoding")
-	if ok && strings.Contains(acceptableEncodings, "gzip") {
 		// Encode the string using the gzip algorithm
-		encodedStr, err := GZip(str)
+		gzStr, err := GZip(str)
 		if err != nil {
 			res.WithStatus(500)
 			return
 		}
-		content = string(encodedStr)
+		str = gzStr // Set the contents to the compressed string
+
+		// Set the response header "Content-Encoding" to "gzip"
 		res.WithHeaders(map[string]string{
 			"Content-Encoding": "gzip",
 		})
+
 	}
 
-	// Set the response status to 200, content type to "text/plain",
-	// content length to the length of the string, and body to the string
+	// Respond with the contents
 	res.
 		WithStatus(200).
 		WithHeaders(map[string]string{
 			"Content-Type":   "text/plain",
-			"Content-Length": fmt.Sprintf("%d", len(content)),
+			"Content-Length": fmt.Sprintf("%d", len(str)),
 		}).
-		WithBody(string(content))
+		WithBody(str)
 
 }
 
-// Return the gzip encoded version of the string
-func GZip(str string) ([]byte, error) {
+// Compress a string using the gzip algorithm.
+func GZip(str string) (string, error) {
 
 	var buf bytes.Buffer       // Create a buffer to store the compressed data
 	gz := gzip.NewWriter(&buf) // Create a new gzip writer
 
 	// Write the string to the gzip writer
 	if _, err := gz.Write([]byte(str)); err != nil {
-		return nil, err
+		return "", err
 	}
 
 	// Close the gzip writer
 	if err := gz.Close(); err != nil {
-		return nil, err
+		return "", err
 	}
 
 	// Flush the gzip writer
-	return buf.Bytes(), nil
+	return buf.String(), nil
+
 }
